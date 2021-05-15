4ebf41c1f8e013d16f60becd74e011684ac33f52
kritebh <kritebhlaganbibhakar@gmail.com>
2021-05-15 19:55:52 +0530

    variables file updated

--- variables(1.1).py
+++ variables(1.1).py
@@ -1,3 +1,4 @@
 # Variables
-name = "Twitter" # Variables are like container and we can change its value anytime
-print(f"Hello,{name}") # formatted, so we can use any variable.
+name = "John"# Variables are like container and we can change its value anytime
+age = 30
+print(f"His name is {name} and his age is {age}") # formatted, so we can use any variable.
