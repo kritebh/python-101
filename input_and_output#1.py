0024eec2ae0b0c65ee008ce0e875c4412b9bb430
kritebh <kritebhlaganbibhakar@gmail.com>
2021-05-15 12:43:08 +0530

    initial commit

---
+++ input_and_output#1.py
@@ -0,0 +1,8 @@
+name = "Twitter"
+print("Hello,Twitter") #We can easily write Your's 
+print ('Hello,Twitter') #We can easily write "quote"
+print('''Hello,Twitter''') #Here we can write anything '' or "" even we can change line
+print(f"Hello,{name}") # formatted, so we can use any variable.
+
+#----------Output--------#
+Hello,Twitter
