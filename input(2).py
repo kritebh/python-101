d7b8f22b95a2997e4687e8bd363e6e8da7d2fead
kritebh <kritebhlaganbibhakar@gmail.com>
2021-05-15 13:12:05 +0530

    Input and Output

--- input(2).py
+++ input(2).py
@@ -1,1 +1,2 @@
+#How to take input from user
 name = input("Enter Your Name : ")
---
+++ variables(1.1).py
@@ -0,0 +1,3 @@
+# Variables
+name = "Twitter" # Variables are like container and we can change its value anytime
+print(f"Hello,{name}") # formatted, so we can use any variable.
