747320429410cfd2326a87bca569c64b041c6724
kritebh <kritebhlaganbibhakar@gmail.com>
2021-05-22 18:56:16 +0530

    Conditional

---
+++ conditional(4).py
@@ -0,0 +1,28 @@
+
+
+## If Condition ##
+weather = "Rainy"
+if weather=="Rainy":
+	print("Take Umbrella")
+## Take Umbrella
+
+
+## If Else Condition ##       
+weather="Clear Sky"
+if weather=="Rainy":
+	print("Take Umbrella")
+else:
+	print("Don't Take Umbrella")
+## Don't Take Umbrella
+## Elif  Condition ##
+a=10
+b=20
+if a==b:
+	print("Both Values are equal")
+elif a>b:
+	print("a is greater")
+elif a<b:
+	print("b is greater")
+else:
+	print("Don't Take Umbrella")
+## b is greater
